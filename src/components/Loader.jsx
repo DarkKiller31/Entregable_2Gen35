bd70b294aa952dacd9a5ae03471c6c0a3ec58c9a
JuanCTrujillo <150824287+DarkKiller31@users.noreply.github.com>
2024-03-02 00:00:00 -0500

    first commit

---
+++ src/components/Loader.jsx
@@ -0,0 +1,12 @@
+import './styles/Loader.css'
+
+const Loader = () => {
+  return (
+    <div className='loader'>
+      <img className="img_loader" src="/carga.gif" alt="" />
+      <h1>Loading...</h1>
+    </div>
+  )
+}
+
+export default Loader
